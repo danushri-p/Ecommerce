2ee0afacc19889b57977d8854cef3b7874bc2420
Danu <danu@prakashs-MacBook-Air.local>
2024-12-03 11:25:33 +0530

    done

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express'); 
+const app = express(); 
+const port = 3001; 
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Backend is running'); 
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
