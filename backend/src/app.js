9f712cbdf109af6b42de4c597a0d49ab116a3d29
Danu <danu@prakashs-MacBook-Air.local>
2024-12-05 16:07:49 +0530

    newest

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+if (process.env.Node_ENV !== 'PRODUCTION'){
+    require('dotenv').config({
+        path: '../src/config/.env',
+    });
+}
+const express = require('express');
+
+const app = express();
+const userRouter = require
+
+app.get('/', (req, res) => {
+    return res.send('Welcome to backend');
+});
+
+
+app.use 
+ module.exports=app;
---
+++ backend/src/utlis/ErrorHandler.js
@@ -0,0 +1,8 @@
+export default class ErrorHandler extends Error{
+    constructor(message,statusCode){
+        super(message);
+        Error.captureStackTrace(this);
+        this.statusCode=statusCode;
+    }
+}
+module.exports = ErrorHandler;
