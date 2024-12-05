f08c6ca793791acfe6dc11fd0369e79f47147e39
danushri-p <danushri.saranyaprakash@gmail.com>
2024-12-05 16:29:12 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,13 @@
-if (process.env.Node_ENV !== 'PRODUCTION'){
+if(process.env.NODE_ENV!=='PRODUCTION'){
     require('dotenv').config({
-        path: '../src/config/.env',
+        path:'./src/config/.env',
     });
 }
-const express = require('express');
-
-const app = express();
-
-const userRouter = require
-
-
-app.get('/', (req, res) => {
-    return res.send('Welcome to backend');
-});
-
-
- module.exports=app;
-
+const express= require('express');
+const app=express()
+const userRouter=
+app.get('/',(req,res)=>{
+    return res.send("Welcome to backend")
+})
+app.use
+module.exports=app;
