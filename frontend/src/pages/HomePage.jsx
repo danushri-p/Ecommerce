f963be1626f8f300717af2aabf5e16158130aa57
Danu <danu@prakashs-MacBook-Air.local>
2025-01-06 17:05:29 +0530

    now

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import Card from '../component/ProductCard/Card';
+import axios from 'axios';
+function HomePage() {
+  const [data, setdata] = useState();
+    const fetchProduct = async () => {
+      const response = await axios.get(
+        'http://localhost:8080/product/get-products'
+       );
+      setdata(response.data.data);
+    };
+    
+useEffect(()=>{
+  console.log('clicked');
+  const callhandle = async()=>{
+    await fetchProduct();
+  };
+  callhandle(); 
+},[]);
+console.log(data);
+
+  return (
+    <div>
+      <h1 className="text-center">'Home Page for Follow along'</h1>
+
+      <div className="grid grid-cols-3">
+        {data?.map((ele, index) => {
+          return (
+            <div style={{ margin: 'auto' }} className="border border-white">
+              <Card 
+              title={ele.title} 
+              image = {ele.image[0] ? ele.image[0]: 'product is missing'} 
+              Index={index} 
+              description = {ele.description}
+              originalPrice = {ele.originalPrice}
+              discountedPrice = {ele.discountedPrice}
+              rating = {ele.rating}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
